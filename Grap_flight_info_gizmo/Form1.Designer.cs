cb4201f0d6c54c7b035822068eb5cf6cf8a97b96
netravler <pheintz@hotmail.com>
2018-09-13 13:16:27 -0500

    Add files via upload

---
+++ Grap_flight_info_gizmo/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Grap_flight_info_gizmo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(823, 26);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Pull History";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(16, 78);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(956, 470);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.WordWrap = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(124, 30);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(673, 22);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "C:\\airportlist\\part139.txt";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 33);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "N-NUMBER";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1027, 673);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Grab Airport Flight Data";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Grap_flight_info_gizmo/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+
+
+namespace Grap_flight_info_gizmo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // examples
+            // http://stackoverflow.com/questions/10826260/is-there-a-way-to-read-from-a-website-one-line-at-a-time
+            // http://social.msdn.microsoft.com/Forums/en-US/1c4cb854-6e32-4d60-be20-bb8ef0218d45/how-can-i-add-lines-in-a-richtextbox-in-c-?forum=winforms
+            
+            //try  // pull data from c:\airportdata\part139.txt csv from FAA.org and parse...
+            //{
+                 // Open the stream and read it back. 
+                string airportData;
+
+                System.IO.StreamReader Airport = new System.IO.StreamReader(@textBox1.Text);
+                
+                while ((airportData = Airport.ReadLine()) != null)
+                {
+                        getTheFlightHistory(airportData);
+                }
+            //}
+            //catch
+            //{
+            //    richTextBox1.AppendText("ERRORERRORERRORERRORERRORERRORERRORERRORERRORERRORERRORERRORERRORERROR");
+            //}
+        }
+
+        private void getTheFlightHistory(String Airport)
+        {
+            /*
+             * Ok in a nutshell we are going to read down through the stream of data and only retain the following lines:
+             * <td><span title="
+             * <td class="nowrap" itemscope itemtype="
+             * <span class="tz">
+             * 
+             * these lines will provide for the only content we will need...
+             * 
+             * code example:
+             * var url ="http://flightaware.com/live/airport/KMCI";
+               var client = new WebClient();
+               using (var stream = client.OpenRead(url))
+               using (var reader = new StreamReader(stream))
+               {
+                    string line;
+                
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        // do stuff
+                    }
+                }
+             */
+
+            // strings to check
+
+            string span = @"<td><span title=";
+            string td = @"<td class=""nowrap"" itemscope itemtype=";
+            string classtz = @"tz";
+
+            var url = "http://flightaware.com/live/airport/" + Airport.Trim();
+            var client = new WebClient();
+            using (var stream = client.OpenRead(url))
+            using (var reader = new StreamReader(stream))
+            {
+                string line;
+
+                while ((line = reader.ReadLine()) != null)
+                {
+                    // do stuff
+                    if (line.Contains(span) | line.Contains(td) | line.Contains(classtz))
+                    {
+                        richTextBox1.AppendText(line + "\n");
+                    }
+                }
+            }
+
+            // close this or it will ball-up
+         }
+
+
+        public static string[] GetStringInBetween(string strBegin, string strEnd, string strSource, bool includeBegin, bool includeEnd)
+        {
+
+            string[] result = { "", "" };
+
+            int iIndexOfBegin = strSource.IndexOf(strBegin);
+
+            if (iIndexOfBegin != -1)
+            {
+
+                // include the Begin string if desired
+
+                if (includeBegin)
+
+                    iIndexOfBegin -= strBegin.Length;
+
+                strSource = strSource.Substring(iIndexOfBegin
+
+                    + strBegin.Length);
+
+                int iEnd = strSource.IndexOf(strEnd);
+
+                if (iEnd != -1)
+                {
+
+                    // include the End string if desired
+
+                    if (includeEnd)
+
+                        iEnd += strEnd.Length;
+
+                    result[0] = strSource.Substring(0, iEnd);
+
+                    // advance beyond this segment
+
+                    if (iEnd + strEnd.Length < strSource.Length)
+
+                        result[1] = strSource.Substring(iEnd
+
+                            + strEnd.Length);
+
+                }
+
+            }
+
+            else
+
+                // stay where we are
+
+                result[1] = strSource;
+
+            return result;
+
+        }
+
+        public static string RemoveBetween(string strBegin, string strEnd, string strSource, bool removeBegin, bool removeEnd)
+        {
+
+            string[] result = GetStringInBetween(strBegin, strEnd, strSource, removeBegin, removeEnd);
+
+            if (result[0] != "")
+            {
+
+                return strSource.Replace(result[0], "");
+
+            }
+
+            // nothing found between begin & end
+
+            return strSource;
+
+        }
+
+        public static string removeStringStuff(string edithold, string keepstring)
+        {
+            string newbigline = "";
+
+            foreach (char spyme in edithold)
+            {
+                if (keepstring.Contains(spyme))
+                {
+                    newbigline += spyme;
+                }
+            }
+
+            return newbigline;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        /* Jack Henry and Associates Jets
+                 
+               http://flightaware.com/live/flight/N894JH
+               http://flightaware.com/live/flight/N156JH
+               http://flightaware.com/live/flight/N152JH
+               http://flightaware.com/live/flight/N157JH
+               http://flightaware.com/live/flight/N156JH
+
+           */
+    }
+
+
+ }
+
